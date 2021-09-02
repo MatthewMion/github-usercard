36282884e36efe5080c3e415e485bf75cdd8f6a0
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-02 11:33:44 -0400

    created card elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,16 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+// const axios = require('axios');
+const URL = 'https://api.github.com/users/MatthewMion'
 
+axios.get(URL)
+  .then(response => {
+    console.log(response)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -33,7 +42,6 @@ const followersArray = [];
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -49,7 +57,26 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(object){
 
+  //create elements
+  const cardDiv = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfoDiv = document.createElement('div')
+  const cardName = document.createElement('h3');
+  const cardUsername = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+  //append
+  cardDiv.appendChild(cardImg);
+
+
+}
 /*
   List of LS Instructors Github username's:
     tetondan
